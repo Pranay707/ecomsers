c105b729c78ddb0d9bc63c34b8def64ddad165a4
Pranay707 <pranaymeshram954@gmail.com>
2025-05-10 22:36:47 +0530

    all done project created

---
+++ src/main/java/com/example/ecomsers/EcomsersApplication.java
@@ -0,0 +1,14 @@
+package com.example.ecomsers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomsersApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(EcomsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ecomsers/EcomsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecomsers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
