4b58c8d34e601612ec7ea26c6f80ac2d1e8abae6
Pranay707 <pranaymeshram954@gmail.com>
2025-05-20 20:36:28 +0530

    error resolved

--- src/main/java/com/example/controller/AdminController.java
+++ src/main/java/com/example/controller/AdminController.java
@@ -32,22 +32,22 @@ public class AdminController {
         return "admin/category";
     }
 
-    @PostMapping("/saveCategory")
-    public String savCategory(@ModelAttribute Category category, HttpSession session){
-        Boolean existCategory = categoryService.existCategory(Category.getName());
-        if (existCategory){
-            session.setAttribute("error", "Category already exist");
-        }
-        else{
-            category saveCategory = categoryService.saveCategory(category);
-            if (ObjectUtils.isEmpty(saveCategory)){
-                session.setAttribute("error", "Category not saved");
-            }
-            else {
-                session.setAttribute("success", "Category saved successfully");
-            }
-        }
-        categoryService.saveCategory(category);
-        return "redirect:/category";
-    }
+//    @PostMapping("/saveCategory")
+//    public String savCategory(@ModelAttribute Category category, HttpSession session){
+//        Boolean existCategory = categoryService.existCategory(Category.getName());
+//        if (existCategory){
+//            session.setAttribute("error", "Category already exist");
+//        }
+//        else{
+//            category saveCategory = categoryService.saveCategory(category);
+//            if (ObjectUtils.isEmpty(saveCategory)){
+//                session.setAttribute("error", "Category not saved");
+//            }
+//            else {
+//                session.setAttribute("success", "Category saved successfully");
+//            }
+//        }
+//        categoryService.saveCategory(category);
+//        return "redirect:/category";
+//    }
 }
