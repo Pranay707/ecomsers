1ee1abe217e69c888b2dca71fde8e9f1aeb3b7d1
Pranay707 <pranaymeshram954@gmail.com>
2025-06-06 19:19:00 +0530

    modified test methods

--- src/main/java/com/example/controller/testController.java
+++ src/main/java/com/example/controller/testController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class testController {
@@ -9,5 +10,10 @@ public class testController {
     @GetMapping("/")
     public String web(){
         return "index";
+    }
+
+    @PostMapping
+    public String name(){
+        return "admin";
     }
 }
